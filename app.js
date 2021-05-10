e57c0137f2467c29ae1cfbcae7ff6dedb0b5df42
Borjaarmet <borjaarmet@gmail.com>
2021-05-10 10:34:22 +0200

    add mongoose

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+mongoose.connect('mongodb://localhost:27017/project-CookHelper', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> {
+  console.log("connected to DB")
+})
+.catch((error)=>{
+  console.log('error', error)
+})
 
 var app = express();
 
