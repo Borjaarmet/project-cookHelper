de155369605cba940b311e0d50e7b7fdd54e8065
Borjaarmet <borjaarmet@gmail.com>
2021-05-10 12:01:29 +0200

    add all GET routes and views.

--- app.js
+++ app.js
@@ -28,16 +28,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const users = require('./routes/users')
+
+app.use('/', users)
+
+const index = require('./routes/index')
+
+app.use('/', index);
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) =>{
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users/signup',(req, res) => {
+  res.render('users/signup')
 });
+
+router.get('/users/login',(req, res) => {
+  res.render('users/login')
+});
+
+router.get('/users/welcome',(req, res) => {
+  res.render('users/welcome')
+});
+
+router.get('/users/profile',(req, res) => {
+  res.render('users/profile')
+});
+
+router.get('/users/search',(req, res) => {
+  res.render('users/search')
+});
+
+router.get('/users/recipes',(req, res) => {
+  res.render('users/recipes')
+});
+
+
+
 
 module.exports = router;
